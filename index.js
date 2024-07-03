959b85f27b3cf630ce0931f6d279d0da93014fe5
Mark Anthony Gutierrez <mr.markgutierrez@gmail.com>
2024-07-02 22:59:44 -0500

    I created the first parts of my project - 3 files

---
+++ index.js
@@ -0,0 +1,24 @@
+// let const declare variables
+
+// update those variables with arithmetic
+
+// logical operators && || 
+
+// control logic (if, else, switch)
+
+// loops (for (#), for of (arrays), while/do while
+
+// get user input (prompt, confirm)
+
+let nums = [9,7,20,11];
+
+console.log(nums[2])
+
+// let item = nums.find(n => n === 20);
+
+// console.log(item)
+
+
+// let bigNums = nums.filter(n => n > 10);
+
+// console.log(bigNums)
