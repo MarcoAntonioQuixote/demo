79847a7aece0bfd59f3094fdeef2ca0ce2627b0d
Mark Anthony Gutierrez <mr.markgutierrez@gmail.com>
2024-07-02 23:01:44 -0500

    Updated a bug in JS file

--- index.js
+++ index.js
@@ -10,13 +10,14 @@
 
 // get user input (prompt, confirm)
 
-let nums = [9,7,20,11];
-
-console.log(nums[2])
 
 // let item = nums.find(n => n === 20);
 
 // console.log(item)
+
+for (let x = 0; x < 100; x++) {
+    console.log(x * 2);
+}
 
 
 // let bigNums = nums.filter(n => n > 10);
